75ccd39813b9a97f1cb371e8be311a02351267bf
RIZNARINUZ <riznarinuz@gmail.com>
2024-09-30 12:30:31 +0530

    Add files via upload

---
+++ risna.py
@@ -0,0 +1,120 @@
+'*'*5
+for value in range(1,6):
+        print('*'*value)
+
+for row in range(1,6):
+    for col in range(0,row):
+          print('*',end="")
+    print("")
+
+for row in range (1,6):
+      for col in range (0,row):
+        print('5',end="")
+      print("")
+
+for row in range(1,6):
+     for col in range(1,row+1):
+        print(row,end="")
+     print("")
+
+list2=[1,21,3,4,5,6]
+index=0
+while index<len(list2):
+     print(list2[index])
+     index=index+1
+
+index=len(list2)-1
+while index>=0:
+      print(list2[index])
+      index=index-1
+
+type(list2)
+
+list4=1,2,3,4,5,6,7,9,9,89
+
+type(list4)
+index=0
+while index<len(list4):
+     print(list4[index])
+     index=index+1
+   
+type(list5)
+
+list5=1,2,3,4,5,6,7,8
+
+type(list5)
+index=0
+while index<len(list5):
+     print(list5[index])
+     index=index+1
+
+list6=1,2,3,4,5,6,7,8
+sum=0
+for value in list6:
+     sum=sum+value
+     
+sum
+
+list7=1,2,3,4,5,6,7,8
+index=0
+sum=0
+while index<len(list7):
+     sum=sum+list7[index]
+     print(sum)
+     index=index+1
+sum
+
+list8=[1,2,3,4,5,6,7,8]
+index=len(list8)-1
+while index>=0:
+     print(list8[index])
+     index=index-1
+
+list8=[1,2,3,4,5,6,7,8]
+index=0
+while index<len(list8):
+     print(list8[index])
+     index=index+1
+
+
+list9=["rizna","shahana","shahala","sheza","shanha","rizin"]
+index=len(list9)-1
+while index>=0:
+     print(list9[index])
+     index=index-1
+
+index9=["rizna","shahana","shahana","shahala","sheza","shanha","rizin"]
+index=0
+while index<len(list9):
+     print(list9[index])
+     index=index+1
+
+list10=[]
+index=0
+while index<10:
+   list10.append(input("enter value"))
+   index=index+1
+print(list10)
+
+list11=[]
+value=input("enter the value")
+while value!='0':
+     list11.append(value)
+     value=input("enter the value")
+print(list11)                                   
+
+list12=[]
+value=input("enter the value")
+while value!='0':
+     list12.append(value)
+     value=input("enter the value")
+print(list12)
+
+list13=[]
+value=input("enter the value")
+while value!='0':
+     list13
+
+
+
+
